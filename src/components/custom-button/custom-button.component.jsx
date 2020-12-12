2988cc0fd393756b87c46cf41f8cbc1f0e04a5ec
Kiwiwuqiao <Kiwiwuqiao@Kiwis-Macbook-Pro.local>
2020-12-13 00:31:10 +0800

    Updates shop data with new data(id), updates cart utils to group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,7 +1,7 @@
 import "./custom-button.styles.scss"
 
-const CustomButton = ({ children, isGoogleSignIn, ...props }) => (
-    <button className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`} {...props}> { children}</button >
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...props }) => (
+    <button className={`${isGoogleSignIn ? "google-sign-in" : ""} ${inverted && "inverted"} custom-button`} {...props}> { children}</button >
 )
 
 export default CustomButton
